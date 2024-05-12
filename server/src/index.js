e1816befbd2776597dce73e7de5e9cd0e7af6f18
Kalana Weranga <kalana@linearsix.com>
2024-05-12 18:35:54 +0530

    change server

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/auth",userRouter);
 app.use("/recipes",recipeRouter);
+const PORT = process.env.PORT || 3001
 
 mongoose.connect(
     "mongodb+srv://kalana:Recipeapp2024@recipes.n8ur4qn.mongodb.net/recipes?retryWrites=true&w=majority&appName=recipes",
@@ -20,4 +21,4 @@ mongoose.connect(
 
 );
 
-app.listen(3001,()=> console.log("server started"))
+app.listen(PORT,()=> console.log("server started"))
